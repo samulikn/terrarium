618780eaa28b869c72c57f3c8aaa3533738a1fb9
Nata <samulikn@gmail.com>
2022-12-10 01:30:46 +0200

    added code to set plant on the front

--- script.js
+++ script.js
@@ -1,58 +1,85 @@
-    'use strict';
+'use strict';
 
-   
-    function dragElement(terrariumElement) {
-        //set 4 positions for positioning on the screen
-        let relX = 0,
-            relY = 0,
-            absX = 0,
-            absY = 0;
+const plants = document.querySelectorAll(".plant");
+let box = document.getElementById('page');
+let width = box.offsetWidth;
+let height = box.offsetHeight;
+//console.log("width", width);
 
-        function pointerDrag(e) {
-            e.preventDefault();
-            absX = e.clientX;
-            absY = e.clientY;
-            document.onpointermove = elementDrag;
-            document.onpointerup = stopElementDrag;
-        }
+plants.forEach(dragElement);
+plants.forEach(setElementToFront);
 
-        function elementDrag(e) {
-            relX = absX - e.clientX;
-            relY = absY - e.clientY;
-            absX = e.clientX;
-            absY = e.clientY;
-            terrariumElement.style.top = terrariumElement.offsetTop - relY + 'px';
-            terrariumElement.style.left = terrariumElement.offsetLeft - relX + 'px';
-        }
+function dragElement(terrariumElement) {
+    //set 4 positions for positioning on the screen
+    let relX = 0,
+        relY = 0,
+        absX = 0,
+        absY = 0;
 
-        function stopElementDrag(s) {
-            document.onpointerup = null;
-            document.onpointermove = null;
-        }
-        
-        terrariumElement.onpointerdown = pointerDrag;
+    function pointerDrag(e) {
+        e.preventDefault();
+        absX = Math.round(e.clientX);
+        absY = Math.round(e.clientY);
+        document.onpointermove = elementDrag;
+        document.onpointerup = stopElementDrag;
     }
 
-      
+    function elementDrag(e) {
+        let rect = terrariumElement.getBoundingClientRect()
 
-;(function() {
-    dragElement(document.getElementById('plant1'));
-    dragElement(document.getElementById('plant2'));
-    dragElement(document.getElementById('plant3'));
-    dragElement(document.getElementById('plant4'));
-    dragElement(document.getElementById('plant5'));
-    dragElement(document.getElementById('plant6'));
-    dragElement(document.getElementById('plant7'));
-    dragElement(document.getElementById('plant8'));
-    dragElement(document.getElementById('plant9'));
-    dragElement(document.getElementById('plant10'));
-    dragElement(document.getElementById('plant11'));
-    dragElement(document.getElementById('plant12'));
-    dragElement(document.getElementById('plant13'));
-    dragElement(document.getElementById('plant14'));
-})()
+        let y = Math.round(e.clientY);
+        relY = absY - y;
+        if ((relY < 0 && (rect.bottom - relY) > height) || (relY > 0 && (rect.top - relY) < 0)) {
+            relY = 0
+        } else {
+            absY = y;
+            terrariumElement.style.top = terrariumElement.offsetTop - relY + 'px';
+        }
 
+        let x = Math.round(e.clientX);
+        relX = absX - x
+        if ((relX < 0 && (rect.right - relX) >= width) || (relX > 0 && (rect.left - relX) <= 0)) {
+            relX = 0;
+        }
+        else {
+            absX = x;
+            terrariumElement.style.left = terrariumElement.offsetLeft - relX + 'px';
+        }
+    }
 
+    function stopElementDrag() {
+        document.onpointerup = null;
+        document.onpointermove = null;
+    }
 
-// 1. Limitate moving plants on the html page
-// 2. set plant on top
+    terrariumElement.onpointerdown = pointerDrag;
+}    
+
+    //
+    function setElementToFront (terrariumElement) {
+        terrariumElement.addEventListener("dblclick", function(){
+            //Create array of all z-indexes 
+            let arrZindexes = [];
+            for (let i = 0; i < plants.length; i++){
+                let z = window.getComputedStyle(plants[i]).getPropertyValue("z-index");
+                arrZindexes.push(z);
+            }
+
+            let currentZindex = window.getComputedStyle(terrariumElement).getPropertyValue("z-index");
+            let maxZindex = FindMaxZindex(arrZindexes);
+            let maxPossibleZindex = 500;
+
+                //Finding the Max z-index
+            function FindMaxZindex(arrZindexes) {
+                return Math.max.apply(null, arrZindexes);
+            }
+
+            if (maxZindex < maxPossibleZindex){
+                let newZindex = maxZindex + 1;
+                terrariumElement.style.zIndex = newZindex;
+            }
+            else {
+                alert ("Reload the page!")
+            }            
+        });
+    };
